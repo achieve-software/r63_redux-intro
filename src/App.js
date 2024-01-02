21aae35e9c64f3daeca55296aa00f9afeaa3320b
Achieve-software <vtcbcdtyr@gmail.com>
2024-01-03 01:55:42 +0300

    redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import "./App.css";
-import Counter from "./components/counter/Counter";
-import Todo from "./components/todo/Todo";
+import "./App.css"
+import Counter from "./components/counter/Counter"
+import Todo from "./components/todo/Todo"
+// import { legacy_createStore as createStore } from "redux"
+// import reducer from "./redux"
+import { Provider } from "react-redux"
+import { store } from "./redux"
 
 function App() {
+  //? Store olusturduk.
+  // const store = createStore(reducer)
+
   return (
     <div className="app">
-      <Counter />
-      <Todo />
+      {/* Store ile tum uygulamayi sarmaladik */}
+      <Provider store={store}>
+        <Counter />
+        <Todo />
+      </Provider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
